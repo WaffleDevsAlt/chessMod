c133099f02f84f33544bb56036c354874bd589ec
WaffleDevsAlt <81845843+WaffleDevsAlt@users.noreply.github.com>
2022-01-21 08:13:42 -0500

    Create mod.js

---
+++ mod.js
@@ -0,0 +1,7 @@
+function moddedLogic(pieceType, pieceColor, id, row, column) {
+  console.log(`Logic: ${pieceType} ${pieceColor} ${id} ${row} ${column}`)
+}
+
+function moddedCheckLogic(pieceType, pieceColor, id, row, column) {
+  console.log(`CheckLogic: ${pieceType} ${pieceColor} ${id} ${row} ${column}`)
+}
