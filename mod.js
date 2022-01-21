e4a86b3c0a2f7c716c242e5a20824705044beff5
WaffleDevsAlt <81845843+WaffleDevsAlt@users.noreply.github.com>
2022-01-21 14:06:39 -0500

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,27 +1,3 @@
-function appendPieceImage(imageSource, containerId, imageId) {
-
-  if (imageSource == 'wrook') imageSource = wrook
-  else if (imageSource == 'wknight') imageSource = wknight
-  else if (imageSource == 'wbishop') imageSource = wbishop
-  else if (imageSource == 'wqueen') imageSource = wqueen
-  else if (imageSource == 'wking') imageSource = wking
-  else if (imageSource == 'wpawn') imageSource = wpawn
-  else if (imageSource == 'brook') imageSource = brook
-  else if (imageSource == 'bknight') imageSource = bknight
-  else if (imageSource == 'bbishop') imageSource = bbishop
-  else if (imageSource == 'bqueen') imageSource = bqueen
-  else if (imageSource == 'bking') imageSource = bking
-  else if (imageSource == 'bpawn') imageSource = bpawn
-  else if (imageSource == 'wtest') imageSource = bking
-  else return;
-
-  var img = document.createElement("IMG");
-  img.src = path + imageSource;
-  img.setAttribute('id', imageId);
-  document.getElementById(containerId).appendChild(img);
-  return imageId;
-}
-
 function moddedLogic(pieceType, pieceColor, id, row, column) {
   if (pieceType == 'king') {
     let knight = [6, 10, 15, 17]
